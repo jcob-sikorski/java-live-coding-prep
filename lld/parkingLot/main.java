884e7798bbe235788813e4e4ed7601e584d761ab
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-02-07 17:04:42 +0100

    init

---
+++ lld/parkingLot/main.java
@@ -0,0 +1,7 @@
+// parking lot is going to be a singleton
+
+// parking lot has a composition of levels, each levek has a composition of parkig spots
+
+// we're going to use an observer pattern so display boards (observers) can know about availability of a each parking spot (subject)
+
+// we can use a strategy pattern for multiple types of ways multiple algorithms that find a parking lot
